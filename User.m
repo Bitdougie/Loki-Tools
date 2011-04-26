3e5338d29a531db115b0cc1749bf8bdd4cd840f3
Douglas Mason <doug@Douglas-Masons-MacBook-Pro.local>
2011-04-27 09:06:14 +1200

    GNU commit

--- User.m
+++ User.m
@@ -2,9 +2,28 @@
 //  User.m
 //  Loki_Tools
 //
-//  Created by Douglas Mason on 11/04/11.
-//  Copyright 2011 Farrand & Mason Ltd. All rights reserved.
-//
+/*
+ Loki Tools a Search engine, data preperation tool that does data mining
+ and retail analysis.
+ Copyright (C) 2011  Douglas Mason
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.//
+ 
+ Loki Tools  Copyright (C) 2011  Douglas Mason
+ This program comes with ABSOLUTELY NO WARRANTY;
+ */
+
 
 #import "User.h"
 
